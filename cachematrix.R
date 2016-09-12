5927f5c907d2f9101894c41031c9070ad85397df
harshadeep2011 <harshadeep2011@gmail.com>
2016-09-12 23:06:46 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Week3-Prgramming Assignment
 
-## Write a short comment describing this function
+## Caching the Inverse of a matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+## Below Function creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) 
+{
+inver <- NULL
+        set <- function(y) {
+                x <<- y
+                inver <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) inver<<- inverse
+        getInverse <- function() inver
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## Below function calculates the inverse of the special "matrix" created by above makeCacheMatrix 
 
-cacheSolve <- function(x, ...) {
+## If the inverse has already been calculated,then it should retrieve the inverse from the cache
+
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+inver<- x$getInverse()
+        if(!is.null(inver)) {
+                message("getting cached data")
+                return(inver)
+        }
+        data <- x$get()
+        inver <- solve(data, ...)
+        x$setInverse(inver)
+        inver
 }
+
+## Testing above Program
+
+ownmatrix<-makeCacheMatrix(matrix(10:13,2,2))
+ownmatrix$get()
+ownmatrix$getInverse()
+cacheSolve(ownmatrix)
